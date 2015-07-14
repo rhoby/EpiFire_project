573bab0b33fe6beb7d73c6e440c8f734778ad95e
Hoby <hoby@aims.edu.gh>
2015-07-14 16:24:51 +0000

    blabla

---
+++ insert-data.sql
@@ -0,0 +1,6 @@
+insert into students('Hoby','Ranarijaona','rhoby', 01);
+insert into students('Samuel', 'Mensah', 'samensah', 02);
+insert into students('Rowena','Cornelius', 'CorneliusR', 03);
+insert into projects(01, 'EpiFire', 'c++');
+insert into projects(02, 'ABC option 1', 'python');
+insert into projects(03, 'ABC option 2', 'c++');
